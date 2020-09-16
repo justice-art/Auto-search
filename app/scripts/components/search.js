fdb8173584cee71393a379ff1abfd0737ea9ede4
Artem Shcherbina <scherbina.ar@gmail.com>
2020-09-16 13:24:39 +0300

    refactor handling api request

--- app/scripts/components/search.js
+++ app/scripts/components/search.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react'
+import { searchCharacters } from '../api/index'
+import { errorHandler } from '../services/functions'
 import useDebounce from './hooks/useDebounce.js'
 
 export default function Search(props) {
   const {setProducts, setLoading, setError} = props;
   const [term, setTerm] = useState('');
   const debouncedTerm = useDebounce(term, 400);
-  const env = 'http://localhost:3035';
 
   useEffect( () => {
       if (debouncedTerm) {
@@ -21,26 +22,13 @@ export default function Search(props) {
     setError('');
     setLoading(true);
     try {
-      const response = await searchCharacters(debouncedTerm);
-      const data = await response.json();
+      const data = await searchCharacters(debouncedTerm);
       setProducts(data);
     } catch(err) {
-      const errorMessage = getErrorMessage(err);
+      const errorMessage = errorHandler(err);
       setError(errorMessage);
     }
     setLoading(false);
-  }
-
-  function searchCharacters(str) {
-    return fetch(`${env}/search?term=${str}`,
-      {
-        method: 'GET'
-      }
-    )
-  }
-
-  function getErrorMessage(err) {
-    return err.message ? err.message : "Check your Internet connection!";
   }
 
   return (
