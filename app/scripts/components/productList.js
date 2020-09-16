29ebca79e86d4dd8e956e21b222ebf33c69cdeab
Artem Shcherbina <scherbina.ar@gmail.com>
2020-09-16 18:25:53 +0300

    add ProductList component

---
+++ app/scripts/components/productList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function ProductList(props) {
+
+  const getTags = (arrayWithTags) => {
+    return arrayWithTags.join(', ');
+  }
+  
+  return (
+    <div className="previews-result">
+      {props.products.map(productItem => (
+          <div key={productItem._id} className="item-preview">
+            <div className="item-preview__image">
+              <img
+                src={productItem.picture}
+              />
+            </div>
+            <div className="item-preview__description">
+              <h4 className="item-preview__name">{productItem.name}</h4>
+              <div className="item-preview__about">{productItem.about}</div>
+              <div className="item-preview__price">${productItem.price}</div>
+              <div className="item-preview__tags">{getTags(productItem.tags)}</div>
+              <a onClick={(e)=> e.preventDefault()} 
+                className={`item-preview__add-to-bag item-preview__add-to-bag_is-active-${productItem.isActive}`}>
+                Add
+              </a>
+            </div>
+          </div>
+        ))}
+    </div>
+  )
+}
--- app/scripts/components/searchContent.js
+++ app/scripts/components/searchContent.js
@@ -1,19 +1,16 @@
 import React, {useState, useEffect} from 'react'
 import Search from './search'
 import Spinner from './spinner'
+import ProductList from './productList'
 
 export default function SearchContent(props) {
   const [products, setProducts] = useState([]);
-  const [presentedProducts, setPresentedProducts] = useState([])
+  const [presentedProducts, setPresentedProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
   const showAllProducts = () => {
     setPresentedProducts(products)
-  }
-
-  const getTags = (arrayWithTags) => {
-    return arrayWithTags.join(', ');
   }
 
   useEffect( () => {
@@ -26,39 +23,16 @@ export default function SearchContent(props) {
       <a href="#" onClick={(e) => props.toggleSearch(e)}>
           <i className="material-icons close">close</i>
       </a>
-      {!loading ? 
-      <div className="previews-result">
-        {presentedProducts.map(productItem => (
-            <div key={productItem._id} className="item-preview">
-              <div className="item-preview__image">
-                <img
-                  src={productItem.picture}
-                />
-              </div>
-              <div className="item-preview__description">
-                <h4 className="item-preview__name">{productItem.name}</h4>
-                <div className="item-preview__about">{productItem.about}</div>
-                <div className="item-preview__price">${productItem.price}</div>
-                <div className="item-preview__tags">{getTags(productItem.tags)}</div>
-                <a onClick={(e)=> e.preventDefault()} 
-                   className={`item-preview__add-to-bag item-preview__add-to-bag_is-active-${productItem.isActive}`}>
-                  Add
-                </a>
-              </div>
-            </div>
-          ))}
-      </div>
-        :
-      <Spinner color='black' isDefault={true} />
-      }
-      { products.length > 2 ?
+      { !loading ? <ProductList products={presentedProducts} /> : <Spinner color='black' isDefault={true} /> }
+      { products.length > 2 &&
         <div className="search-content__all-products all-products">
           <span className="all-products__title">
             {`Displaying ${presentedProducts.length} of ${products.length} results`}
           </span>
-          {presentedProducts.length === products.length || <a className="all-products__button" onClick={showAllProducts}>Display all results</a>}
-        </div> : null }
-        { error && 
+          {presentedProducts.length !== products.length && <a className="all-products__button" onClick={showAllProducts}>Display all results</a>}
+        </div>
+       }
+      { error && 
         <div className="search-content__err">
           {error}
         </div>
